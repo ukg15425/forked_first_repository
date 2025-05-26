4108be856841fb6727c36f891f1e0a70bd1f9cf7
ukg15425 <ukg15425@kireygroup.com>
2025-05-26 16:33:30 +0200

    aggiunto esecuzione uvicorn da python

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -7,3 +8,12 @@ app = FastAPI()
 async def home():
     # changes
     return "furia balcanica"
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        app, 
+        host='127.0.0.1', 
+        port=8000, 
+        reload=True
+    )
